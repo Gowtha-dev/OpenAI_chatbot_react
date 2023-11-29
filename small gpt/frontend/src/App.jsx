867d1158fb021f1d96e246d9e8ec1a27e590203b
Gowtha-dev <56531397+Gowtha-dev@users.noreply.github.com>
2023-11-30 03:32:17 +0530

    init commit

---
+++ small gpt/frontend/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chats, setChats] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    if (!message) return;
+    setIsEditing(true);
+    scrollTo(0, 1e10);
+
+    let msgs = chats;
+    msgs.push({ role: "user", content: message });
+    setChats(msgs);
+
+    setMessage("");
+
+    fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        chats,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        msgs.push(data.output);
+        setChats(msgs);
+        setIsEditing(false);
+        scrollTo(0, 1e10);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <main>
+      <h3>Small GPT</h3>
+
+      <section>
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>{chat.role.toUpperCase()}</b>
+                </span>
+                <span>:</span>
+                <span>{chat.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+      <div className={isEditing ? "" : "hide"}>
+        <p>
+          <i>{isEditing ? "Fetching From GPT" : ""}</i>
+        </p>
+      </div>
+
+      <form action="" onSubmit={(e) => chat(e, message)}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          placeholder="Type your query and hit me with enter..."
+          onChange={(e) => setMessage(e.target.value)}
+          style={{color:'white'}}
+        />
+      </form>
+    </main>
+  );
+}
+
+export default App;
